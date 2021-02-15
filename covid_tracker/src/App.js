f518d65104ac507f76f2b4df90f3dd81f6d3d9c7
matthewromano5087 <matthewromano5087@gmail.com>
2021-02-15 13:02:30 -0500

    Responsiveness/CSS

--- covid_tracker/src/App.js
+++ covid_tracker/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 import {fetchData} from './api';
+import covidImage from './images/covid19.png'
 
 class App extends React.Component {
     state = {
@@ -16,16 +17,19 @@ class App extends React.Component {
     }
 
     handleCountryChange = async (country) => {
-        
+        const fetchedData = await fetchData(country);
+
+        this.setState({data: fetchedData, country: country});
     }
 
     render() {
-        const {data} = this.state;
+        const {data, country} = this.state;
         return (
             <div className={styles.container}>
+                <img className={styles.images} src={covidImage} alt="Covid-19"/>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart />
+                <Chart data={data} country={country} />
             </div>
         )
     }
--- covid_tracker/src/components/CountryPicker/CountryPicker.jsx
+++ covid_tracker/src/components/CountryPicker/CountryPicker.jsx
@@ -17,7 +17,7 @@ const CountryPicker = ({hnadleCountryChange}) => {
     return (
         <FormControl className={styles.formControl}>
             <NativeSelect defaultValue="" onChange={(e) => hnadleCountryChange(e.target.value)}>
-                <option value="global">Global</option>
+                <option value="">Global</option>
                 {fetchedCountries.map((country) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
