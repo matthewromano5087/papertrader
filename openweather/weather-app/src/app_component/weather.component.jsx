478e8403f81b5edcf19483735c64d4ab99a27b5b
matthewromano5087 <matthewromano5087@gmail.com>
2021-02-17 18:08:40 -0500

    weather jsx

---
+++ openweather/weather-app/src/app_component/weather.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../App';
+
+
+const Weather = () => {
+    return (
+        <div className="container">
+            <h1>Weather App</h1>
+        </div>
+    );
+};
+
+export default Weather;
